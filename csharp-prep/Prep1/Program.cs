0cdd7f13cebd7b7b9dacfaa149fa51e180820d31
kayla-j-davies <kaylajadedavies19@gmail.com>
2024-01-17 16:19:16 +0200

    Added questions for inout and output.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ") ;
+        string first = Console.ReadLine();
+
+        Console.Write("What is your second name? ");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is (last_name), (first_name) (last_name).");
     }
 }
