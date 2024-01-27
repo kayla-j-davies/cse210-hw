850adc7052a1c33aa05091de3878c68498e406e0
kayla-j-davies <kaylajadedavies19@gmail.com>
2024-01-27 18:20:11 +0200

    Wrote the program for working with a number list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int uNumber = -1;
+        while (uNumber != 0) 
+        {
+            Console.Write("Entwr number: ");
+            string userAnswer = Console.ReadLine();
+            uNumber = int.Parse(userAnswer);
+
+            if (uNumber != 0)
+            {
+                numbers.Add(uNumber);
+            }
+
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int maximum = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+        
+        Console.WriteLine($"The largest number is: {maximum}");
+
     }
 }
