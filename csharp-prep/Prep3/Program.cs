d362845da3499be82d1bf3127e86a9f78d86b532
kayla-j-davies <kaylajadedavies19@gmail.com>
2024-01-27 18:16:44 +0200

    Wrote the number guessing game code.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1,101);
+
+//        Console.Write("What is the magic number? ");
+//        string answer = Console.ReadLine();
+//        int mNumber = int.Parse(answer);
+//
+        int gNumber = -1;
+
+        while (gNumber != mNumber)
+        {
+            Console.Write("What is your guess? ");
+            gNumber = int.Parse(Console.ReadLine());
+
+            if (mNumber > gNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (mNumber < gNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+         
+
+        
+
     }
 }
