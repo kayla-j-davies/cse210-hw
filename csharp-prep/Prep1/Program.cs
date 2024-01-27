b85a3bb90041ee10841eb0ec0ecbd03db5382634
kayla-j-davies <kaylajadedavies19@gmail.com>
2024-01-27 18:11:53 +0200

    Fixed code to make program work.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,9 +9,9 @@ class Program
         Console.Write("What is your first name? ") ;
         string first = Console.ReadLine();
 
-        Console.Write("What is your second name? ");
-        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
 
-        Console.WriteLine($"Your name is (last_name), (first_name) (last_name).");
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
